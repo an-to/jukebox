6de5fcb570a771beb3bbfe01455ed753bd6b21d8
Alan Jordan <alanpjordan@gmail.com>
2017-06-02 13:28:46 +1200

    Going to merge to dev

--- test/client/index.test.js
+++ test/client/index.test.js
@@ -1,37 +1,37 @@
-// import test from 'ava'
-// import React from 'react'
-// import { shallow, mount, render } from 'enzyme'
-// import * as sinon from 'sinon'
-//
-// import './setup-dom'
-//
-// import App from '../../client/components/App'
-//
-// App.prototype.componentDidMount = () => {}
-//
-// test('playTrack function fires when play link clicked', t => {
-//   const wrapper = mount(<App />)
-//
-//   const app = wrapper.instance()
-//   sinon.stub(app, 'playTrack')
-//
-//   wrapper.find('#playTrack').simulate('click')
-//   t.is(app.playTrack.called, true)
-// })
-//
-// test('pauseTrack function fires when play link clicked', t => {
-//   const wrapper = mount(<App />)
-//
-//   const app = wrapper.instance()
-//   sinon.stub(app, 'pauseTrack')
-//
-//   wrapper.find('#pauseTrack').simulate('click')
-//   t.is(app.pauseTrack.called, true)
-// })
+import test from 'ava'
+import React from 'react'
+import { shallow, mount, render } from 'enzyme'
+import * as sinon from 'sinon'
 
-//
-// test('<App />', t => {
-//   const expected = 'React development has begun!'
-//   const wrapper = mount(<App />)
-//   t.is(wrapper.text(), expected)
-// })
+import './setup-dom'
+
+import App from '../../client/components/App'
+
+App.prototype.componentDidMount = () => {}
+
+test('playTrack function fires when play link clicked', t => {
+  const wrapper = mount(<App />)
+
+  const app = wrapper.instance()
+  sinon.stub(app, 'playTrack')
+
+  wrapper.find('#playTrack').simulate('click')
+  t.is(app.playTrack.called, true)
+})
+
+test('pauseTrack function fires when play link clicked', t => {
+  const wrapper = mount(<App />)
+
+  const app = wrapper.instance()
+  sinon.stub(app, 'pauseTrack')
+
+  wrapper.find('#pauseTrack').simulate('click')
+  t.is(app.pauseTrack.called, true)
+})
+
+
+test('<App />', t => {
+  const expected = 'React development has begun!'
+  const wrapper = mount(<App />)
+  t.is(wrapper.text(), expected)
+})
