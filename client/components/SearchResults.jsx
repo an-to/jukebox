ffa20386400b6b8293fe8fc33058e69b66eaea39
Sherly Chan <sherlyc@gmail.com>
2017-06-02 12:28:29 +1200

    working on css of display results

--- client/components/SearchResults.jsx
+++ client/components/SearchResults.jsx
@@ -4,12 +4,19 @@ import {connect} from 'react-redux'
 
 let SearchResults = (props) => (
   <div className='searchResults'>
-      Search Result
     {props.searchResults.map( (result) =>
-
-      <div>{result.title}</div>
-
-     )}
+     <div className="track">
+         <div className="trackThumbnail">
+             <a href={result.permalink_url}><img src={result.artwork_url}/></a>
+         </div>
+         <div className="trackDescription">
+             <a href={result.permalink_url}><span>{result.title}</span></a>
+         </div>
+         <div className="trackAction">
+             <a href='#'>Add</a>
+         </div>
+     </div>
+      )}
   </div>
 )
 const mapState2Props = (state) => {
